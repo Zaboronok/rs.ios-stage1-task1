7d06f03a66078ccb08cfa56e2fb84ecbaf6ed384
Yahor <egorzaboronok@gmail.com>
2020-03-16 12:31:48 +0300

    feat: done exercises 1,2,3

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,7 +4,24 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    if ([sadArray count] != nil) {
+        NSMutableArray *newArray = [[NSMutableArray arrayWithArray:sadArray]init];
+        for (int x=0; x<3; x++) {
+            for (int i = 1; i < [newArray count] - 1; i++) {
+                int prev = [(NSNumber *)[newArray objectAtIndex:i - 1] intValue];
+                int next = [(NSNumber *)[newArray objectAtIndex:i + 1] intValue];
+                int sum = prev + next;
+                int curNum = [(NSNumber *)[newArray objectAtIndex:i] intValue];
+                //NSLog(@"prev:%d \n next:%d \n current number: %d \n sum: %d",prev,next,curNum,sum);
+                if (curNum  > sum) {
+                    [newArray removeObjectAtIndex:i];
+                }
+            }
+        }
+        sadArray = [NSArray arrayWithArray:newArray];
+        //NSLog(@"sadArray: %@",sadArray);
+    }
+    return [NSArray arrayWithArray:sadArray];
 }
 
 @end
